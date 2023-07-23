a04107806621ecfe3930853c1be76fbf14a7aee1
Starkoiii <131171379+Starkoiii@users.noreply.github.com>
2023-07-23 14:51:26 -0500

    edited generate markdown file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,21 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  [![License: ${data.license}](https://img.shields.io/badge/License-${data.license}-green.svg)](https://opensource.org/licenses/${data.license})
+  ## Description\n${data.description}
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [How To Contribute](#contributions)
+  - [Tests](#testing)
+  ## Installation\n\n${data.installation}\n
+  ## Usage\n\n${data.usage}\n
+  ## License\n\nThis project is licensed under ${data.license}.
+  ## How To Contribute\n\n${data.contributions}\n
+  ## Tests\n\n${data.testing}
+  ## Question\n If you have any questions, please reach out to my github account: [Github](https://github.com/${data.github}), or my email: [${data.contact}](${data.contact})
 `;
 }
 
