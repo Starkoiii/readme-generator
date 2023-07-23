7941d1a8a60778dc01abca779f23e9628498c764
Starkoiii <131171379+Starkoiii@users.noreply.github.com>
2023-07-23 14:45:17 -0500

    created function for readme file and edited index.js file

--- index.js
+++ index.js
@@ -1,13 +1,72 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const markDown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
-
+function questions() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: 'What is your title of your Project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is the description of your Project?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the Installation instructions for your application?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'How should a user use your application?',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        message: 'What License do you have for your project',
+        choices: ['MIT', 'Apache_2.0', 'MPL_2.0', 'ISC'],
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'What are the guidelines for Contributing to your project?',
+        name: 'contributions',
+    },
+    {
+        type: 'input',
+        message: 'What instructions do you have for testing?',
+        name: 'testing',
+    },
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'contact',
+    },
+  ])
+    .then((response) =>
+      writeToFile(markDown(response))
+      );
+}
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile('README.md', `${data}\n`, (err) =>
+  err ? console.log(err) : console.log('Success!'))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { 
+  questions()
+}
 
 // Function call to initialize app
 init();
